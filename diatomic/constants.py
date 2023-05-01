25742840bd0a8c08aaf35205b3f22d5a02de9def
qw372 <qian.wang.372@gmail.com>
2023-05-01 12:07:55 -0500

    dunham series

--- diatomic/constants.py
+++ diatomic/constants.py
@@ -1,4 +1,5 @@
-import scipy.constants
+import numpy as np
+from scipy.constants import speed_of_light
 from dataclasses import dataclass
 
 __all__ = ['SrFConstants']
@@ -13,8 +14,8 @@ class MolecularConstants:
     NuclearSpin_I: float # nuclear spin, assume only one nucleus has non-zero spin
     ElectronSpin_S: float # electronic spin
     DipoleMoment_d: float # Debye, body-frame dipole moment
-    RotationalConstant_B: float # Hz, defined as E/h
-    CentrifugalDistortion_D: float # Hz, defined as E/h
+    RotationalConstant_B: np.ndarray # Hz, defined as E/h, expressed in Dunham series in order of [[Y_00, Y_01, Y_02, ...], [Y_10, Y_11, Y_12, ...], ...]
+    # CentrifugalDistortion_D: float # Hz, defined as E/h
     SpinRotationalCoupling_gamma: float # Hz, defined as E/h
     HyperfineCoupling_b: float # Hz, defined as E/h
     DipoleDipoleCoupling_c: float # Hz, defined as E/h
@@ -22,12 +23,15 @@ class MolecularConstants:
 
 
 # See John Barry's thesis chapter 2.4, 2.5 for details
-SrFConstants = MolecularConstants(NuclearSpin_I=1/2,
-                                  ElectronSpin_S=1/2,
-                                  DipoleMoment_d=3.4963,
-                                  RotationalConstant_B=7.5108318e9,
-                                  CentrifugalDistortion_D=7482.4,
-                                  SpinRotationalCoupling_gamma=75.02249e6,
-                                  HyperfineCoupling_b=97.6670e6,
-                                  DipoleDipoleCoupling_c=29.846e6,
-                                  NuclearSpinRotationalCoupling_C=0.00230e6)
+SrFConstants = MolecularConstants(NuclearSpin_I = 1/2,
+                                  ElectronSpin_S = 1/2,
+                                  DipoleMoment_d = 3.4963,
+                                  RotationalConstant_B = np.array([[0, 0.250534383, -2.49586e-7, -3.30e-14]
+                                                                   [501.96496, -1.551101e-3, -2.423e-10],
+                                                                   [-2.204617, 2.1850e-6, 1.029e-11],
+                                                                   [5.2815e-3, 1.518e-8]])*1e2*speed_of_light,
+                                #   CentrifugalDistortion_D = 7482.4,
+                                  SpinRotationalCoupling_gamma = 75.02249e6,
+                                  HyperfineCoupling_b = 97.6670e6,
+                                  DipoleDipoleCoupling_c = 29.846e6,
+                                  NuclearSpinRotationalCoupling_C = 0.00230e6)
